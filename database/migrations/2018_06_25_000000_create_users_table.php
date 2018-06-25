9aeefff90af39083dd24886dfedc70d164165b5a
Brandon Plentl <rplentl@softlayer.com>
2018-06-25 12:21:13 -0500

    Added some Migrations

--- database/migrations/2018_06_25_000000_create_users_table.php
+++ database/migrations/2018_06_25_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password', 255);
+            $table->tinyinteger('role')->default(0);
+            $table->tinyinteger('status')->default(0);
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2018_06_25_134838_create_user_info_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_info', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('user_id');
+            $table->string('name', 255)->nullable();
+            $table->string('display_name', 255)->nullable();
+            $table->string('bio', 255)->nullable();
+            $table->string('social_meta', 255)->nullable();
+            $table->ipAddress('ip_address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_info');
+    }
+}
