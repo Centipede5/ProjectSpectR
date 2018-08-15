6c35cf91fb54ac967f46a24c52552432bfd851e8
Brandon Plentl <bplentl@gmail.com>
2018-08-15 10:47:28 -0500

    Adjusted the uniqid and added the default image titles here. I could've created these images as the default in the table record, but I want to setup a random assignment here some day.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,10 +70,12 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         $user = User::create([
-            'display_name'  => $data['display_name'],
-            'email'         => $data['email'],
-            'password'      => bcrypt($data['password']),
-            'uniqid'        => uniqid() . mt_rand(100, 999),
+            'display_name'      => $data['display_name'],
+            'email'             => $data['email'],
+            'password'          => bcrypt($data['password']),
+            'uniqid'            => uniqid("..") . mt_rand(100, 999),
+            'profile_image'     => "00-default-profile.jpg",
+            'background_image'  => "00-default-background.jpg",
         ]);
 
         $user->roles()->attach(1);
