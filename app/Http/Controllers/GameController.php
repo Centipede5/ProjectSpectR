5afe4514874cc5e5c829f077f1779f6c50237282
Brandon Plentl <rplentl@softlayer.com>
2018-09-14 15:51:07 -0500

    First edition of the GameController. Just returns all Games right now.

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -7,5 +7,29 @@ use App\Game;
 
 class GameController extends Controller
 {
-    //
+
+    /**
+     * @param string $filter
+     * @param int $limit
+     * @return Game[]|\Illuminate\Database\Eloquent\Collection
+     *
+     *  Currently release_date_na has an accessor defined, but can be overrode.
+     *  in the view, use {{$game->getOriginal('release_date_na')}}
+     *  This will display the stored DB value
+     */
+    public function getGames($filter='all', $limit=100) {
+
+        $currentDate = date('Y-m-d');
+
+        if ($filter=='home-recent'){
+            $games = Game::where('release_date_na', '>=', $currentDate)
+                ->orderBy('release_date_na')
+                ->take(6)
+                ->get();
+        } else {
+            $games = Game::all();
+        }
+
+        return $games;
+    }
 }
