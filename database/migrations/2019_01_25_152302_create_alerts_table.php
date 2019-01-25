07e7b308fd9600e163b11228c7fd33eeda470979
Brandon Plentl <rplentl@softlayer.com>
2019-01-25 09:53:46 -0600

    Adding the base Alert Model, Controller and Migrations

---
+++ database/migrations/2019_01_25_152302_create_alerts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('status')->default(0);
+            $table->integer('severity')->default(0);
+            $table->string('title', 100);
+            $table->string('message', 255);
+            $table->boolean('send_email')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alerts');
+    }
+}
