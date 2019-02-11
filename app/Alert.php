b422749276ab2bd74c4f6c823a1f797cc20addf2
Brandon Plentl <rplentl@softlayer.com>
2019-02-11 14:57:15 -0600

    Continued work on the Alert Controller

--- app/Alert.php
+++ app/Alert.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alert extends Model
 {
-    //
+    protected $guarded = ['id'];
+//    protected $fillable = [
+//        'status',
+//        'severity',
+//        'title',
+//        'message',
+//        'send_email'
+//    ];
 }
