378f0144716c39074afa050c34c1e9ddaca5edde
Brandon Plentl <rplentl@softlayer.com>
2019-02-12 14:34:41 -0600

    Continued work on the Alert Controller

--- app/Http/Controllers/AlertController.php
+++ app/Http/Controllers/AlertController.php
@@ -7,14 +7,13 @@ use App\Alert;
 
 class AlertController extends Controller
 {
-    public function addAlert($status=9,$severity=0, $send_email=0){
-        $title = "Test Alert 3";
-        $message = "This is a test message!";
-        $data['status']=$status;
-        $data['severity']=$severity;
-        $data['title']=$title;
-        $data['message']=$message;
-        $data['send_email']=$send_email;
+    public function addAlert($title, $message, $status=0, $severity=0, $send_email=0){
+        $data['title'] = $title;
+        $data['message'] = $message;
+        $data['status'] = $status;
+        $data['severity'] = $severity;
+        $data['send_email'] = $send_email;
+
         Alert::create($data);
     }
 }
